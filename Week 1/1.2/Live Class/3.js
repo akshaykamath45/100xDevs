b84f25772991de6aabe9714a78c8581df7a61eeb
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-03 20:42:47 +0530

    call back function

--- Week 1/1.2/Live Class/3.js
+++ Week 1/1.2/Live Class/3.js
@@ -1,5 +1,25 @@
-function sum(a,b){
-    return a+b;
+function sum(a, b) {
+    return a + b;
 }
-const value=sum(4,5);
+const value = sum(4, 5);
 console.log(value);
+
+// call back function 
+function sum1(num1, num2, fnToCall) {
+    let result = num1 + num2;
+    fnToCall(result)
+    return result;
+}
+
+function displayResult(data) {
+    console.log("Result of the sum is : " + data);
+}
+
+function displayResultPassive(data) {
+    console.log("Sum's result is : " + data);
+}
+
+
+// You are only allowed to call one function after this
+// How will you displayResult of a sum
+sum1(4, 5, displayResult);
