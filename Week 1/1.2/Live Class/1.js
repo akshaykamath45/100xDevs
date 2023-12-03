acdc7f035d0a6b01529becd9879ac4ad2a38c38c
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-03 19:46:37 +0530

    variables

---
+++ Week 1/1.2/Live Class/1.js
@@ -0,0 +1,14 @@
+// using var
+var a = 2;
+a = 3;
+console.log(a);
+
+// using let
+let b = 4;
+b = 5;
+console.log(b);
+
+// using const
+const c=45;
+// c=18; reassignment is not possible for const , value won't change , to be on safer side.
+console.log(c);
