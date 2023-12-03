7668c0d4204ff8db0fd854d6805d3c814be96beb
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-03 20:00:11 +0530

    for loop

--- Week 1/1.2/Live Class/2.js
+++ Week 1/1.2/Live Class/2.js
@@ -10,3 +10,16 @@ if (isLearning == true) {
 } else {
     console.log(firstName + " is not learning.");
 }
+
+
+// loops
+// for (let i=0;i<=1000;i++){
+//     console.log("People to answer "+i);
+// }
+
+// calculating sum up to n using for loop  
+let answer=0
+for(let i=0;i<=1000;i++){
+    answer+=i;
+}
+console.log(answer)
