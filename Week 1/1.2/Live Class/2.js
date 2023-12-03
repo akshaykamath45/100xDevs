536f91e03dd52b486d3cfa4de030d836a45fda07
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-03 19:53:09 +0530

    if else

--- Week 1/1.2/Live Class/2.js
+++ Week 1/1.2/Live Class/2.js
@@ -2,3 +2,11 @@ let firstName = "Akshay";
 let age = 20;
 let isLearning = true;
 console.log("This is " + firstName + ",Age is " + age + ".");
+
+
+// if else
+if (isLearning == true) {
+    console.log(firstName + " is learning.");
+} else {
+    console.log(firstName + " is not learning.");
+}
