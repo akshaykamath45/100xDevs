95e5a742a62454130d4f59393dc6c67db61be7d0
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-05 17:27:21 +0530

    completed count vowels

--- Week 1/Assignment/01-js/medium/countVowels.js
+++ Week 1/Assignment/01-js/medium/countVowels.js
@@ -5,8 +5,54 @@
   Once you've implemented the logic, test your code by running
 */
 
+// function countVowels(str) {
+//   let count=0;
+//   for(let i=0;i<str.length;i++){
+//     if(str[i]==='a'){
+//       count+=1
+//     }
+//     else if(str[i]==='e'){
+//       count+=1
+//     }
+//     else if(str[i]==='i'){
+//       count+=1
+//     }
+//     else if(str[i]==='o'){
+//       count+=1
+//     }
+//     else if(str[i]==='u'){
+//       count+=1
+//     }
+//     else if(str[i]==='A'){
+//       count+=1
+//     }
+//     else if(str[i]==='E'){
+//       count+=1
+//     }
+//     else if(str[i]==='I'){
+//       count+=1
+//     } else if(str[i]==='O'){
+//       count+=1
+//     }
+//     else if(str[i]==='U'){
+//       count+=1
+//     }
+//   }
+//   return count
+// }
+
+// better way to implement
 function countVowels(str) {
-    // Your code here
+  const vowels = new Set('aeiouAEIOU');
+  let count = 0;
+  for (let char of str) {
+    if (vowels.has(char)) // checks if char exists in vowels
+    {
+      count += 1
+    }
+  }
+  return count
+
 }
 
 module.exports = countVowels;
