91f054b210f9ac14382f18fe29d098389bee028d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-05 14:51:24 +0530

    anagram complete

--- Week 1/Assignment/01-js/easy/anagram.js
+++ Week 1/Assignment/01-js/easy/anagram.js
@@ -5,7 +5,36 @@
 */
 
 function isAnagram(str1, str2) {
+  let ans1 = []
+  let ans2 = []
 
+  for (let i = 0; i < str1.length; i++) {
+    ans1.push(str1.charCodeAt(i))
+  }
+  let sortAns1 = ans1.sort((a, b) => a - b)
+  console.log(sortAns1)
+  console.log(sortAns1.length);
+
+  for (let i = 0; i < str2.length; i++) {
+    ans2.push(str2.charCodeAt(i))
+  }
+
+  let sortAns2 = ans2.sort((a, b) => a - b)
+  console.log(sortAns2);
+  console.log(sortAns2.length);
+
+  if (sortAns1.length == 0 && sortAns2.length == 0) {
+    return true
+  }
+
+  for (let i = 0; i < sortAns1.length; i++) {
+    if (sortAns1.length === sortAns2.length) {
+      if (sortAns2[i] == sortAns1[i]) {
+        return true
+      }
+    }
+    return false
+  }
 }
 
 module.exports = isAnagram;
