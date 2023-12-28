eb182ace1c8492655afb78d2df04c2c17a9dbd29
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-12-28 16:25:26 +0530

    sample express server

--- Week 2/2.2/Live Class/server.js
+++ Week 2/2.2/Live Class/server.js
@@ -2,7 +2,15 @@ const express=require("express")
 
 const app=express()
 
+
+app.get("/route-handler",(req,res)=>{
+    res.json({
+        "name":"Akshay",
+        "age":20
+    })
+})
 app.get("/",(req,res)=>{
+    // req->headers,body,query params
     res.send("Hello server is working fine")
 })
 
