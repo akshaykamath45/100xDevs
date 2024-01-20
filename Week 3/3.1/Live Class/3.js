43a97e828fd8c320375e910ef899b01227113d59
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-21 00:26:10 +0530

    validating user

--- Week 3/3.1/Live Class/3.js
+++ Week 3/3.1/Live Class/3.js
@@ -9,3 +9,22 @@ function validateInput(arr){
 }
 
 validateInput(["12","13"])
+
+
+const userSchema=zod.object({
+    email:zod.string().email(),
+    password:zod.string().min(5),
+    country:zod.literal("IN").or(zod.literal("US"))
+})
+function validateUser(user){
+    const response=userSchema.safeParse(user)
+    console.log(response)
+}
+
+const user={
+    email:"sample@gmail.com",
+    password:"12345",
+    country:"IN"
+}
+
+validateUser(user)
