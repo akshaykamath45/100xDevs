26dcf7d846e0c1552758b647c1a5f76d2ca8a93a
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-01-21 16:26:46 +0530

    completed rate limitter

--- Assignments/week-3/01-middlewares/dist/02-ratelimitter.dev.js
+++ Assignments/week-3/01-middlewares/dist/02-ratelimitter.dev.js
@@ -1,8 +1,12 @@
-const request = require("supertest");
-const assert = require("assert");
-const express = require("express");
-const app = express();
-// You have been given an express server which has a few endpoints.
+"use strict";
+
+var request = require("supertest");
+
+var assert = require("assert");
+
+var express = require("express");
+
+var app = express(); // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
 // rate limit the requests from a user to only 5 request per second
 // If a user sends more than 5 requests in a single second, the server
@@ -11,38 +15,46 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
-let numberOfRequestsForUser = {};
-const rateLimitter = (req, res, next) => {
-  const userId = req.headers["user-id"];
+var numberOfRequestsForUser = {};
+
+var rateLimitter = function rateLimitter(req, res, next) {
+  var userId = req.headers["user-id"];
+
   if (!userId) {
-    res.status(404).json({ msg: "User not found" });
+    res.status(404).json({
+      msg: "User not found"
+    });
   }
+
   numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
   numberOfRequestsForUser[userId] += 1;
   console.log(numberOfRequestsForUser[userId]);
+
   if (numberOfRequestsForUser[userId] > 5) {
-    return res.status(404).json({ msg: "Excedded number of requests (5)" });
+    return res.status(404).json({
+      msg: "Excedded number of requests (5)"
+    });
   }
+
   next();
 };
 
 app.use(rateLimitter);
-
-setInterval(() => {
+setInterval(function () {
   numberOfRequestsForUser = {};
 }, 5000);
-
 app.get("/user", function (req, res) {
-  res.status(200).json({ name: "john" });
+  res.status(200).json({
+    name: "john"
+  });
 });
-
 app.post("/user", function (req, res) {
-  res.status(200).json({ msg: "created dummy user" });
+  res.status(200).json({
+    msg: "created dummy user"
+  });
 });
-
-let PORT = 3000;
+var PORT = 3000;
 app.listen(PORT, function () {
-  console.log(`Server is listening on port ${PORT}`);
+  console.log("Server is listening on port ".concat(PORT));
 });
-
 module.exports = app;
