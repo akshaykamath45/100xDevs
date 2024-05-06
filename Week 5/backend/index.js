ee87e0608dfa6a52a3ca331db9921d0330c3e407
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-05-06 16:49:37 +0530

    feat: adding zod validation

--- Week 5/backend/index.js
+++ Week 5/backend/index.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 app.use(express.json());
-
 const PORT = 3000 || process.env.PORT;
+
 app.get("/", async (req, res) => {
   res.send("Backend server working!");
 });
 
+app.post("/todo", async (req, res) => {
+  const todoBody = req.body;
+  const parsedBody = createTodo.safeParse(todoBody);
+  if (!parsedBody.success) {
+    res.status(411).json("You have send wrong inputs");
+    return;
+  }
+  // add mongodb logic 
+});
+
+app.get("/todos", async (req, res) => {
+  // directly add mongodb logic
+});
+app.put("/completed", async (req, res) => {
+  const updateTodoBody = req.body;
+  const parsedBody = updateTodo.safeParse(updateTodoBody);
+  if (!parsedBody.success) {
+    res.status(411).json("You have send wrong inputs");
+    return;
+  }
+  // add mongodb logic
+});
 app.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`);
 });
