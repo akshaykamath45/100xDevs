bd4136c2b2bd3bc60dad7cf8e6c7951d86c19fc7
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-05-06 18:37:05 +0530

    fix: adding cors

--- Week 5/backend/index.js
+++ Week 5/backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { Todo } = require("./db");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 const PORT = 3000 || process.env.PORT;
 
 app.get("/", async (req, res) => {
